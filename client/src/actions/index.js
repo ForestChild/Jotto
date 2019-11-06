366350f8514a34ad3e3e9fbdd06fe083cb66caf3
Cole Park <colepark2@outlook.com>
2019-11-05 20:54:02 -0600

    Add guessWord action creator tests, write code for secretWordReducer and guessWord action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,10 @@
 export const actionTypes = {
 	CORRECT_GUESS: "CORRECT_GUESS",
-	GUESS_WORD: "GUESS_WORD"
-}
+	GUESS_WORD: "GUESS_WORD",
+	SET_SECRET_WORD: "SET_SECRET_WORD"
+};
+
+import axios from "axios";
 import { getLetterMatchCount } from "../helpers";
 
 /**
@@ -34,3 +37,15 @@ export const guessWord = (guessedWord) => {
 		}
 	};
 };
+
+export const getSecretWord = () => {
+	return (dispatch) => {
+		return axios.get("http://localhost:3030")
+			.then(response => {
+				dispatch({
+					type: actionTypes.SET_SECRET_WORD,
+					payload: response.data
+				});
+			});
+	}
+}
