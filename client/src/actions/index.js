3f1d1feb845ffbdf63c56b6239a498d952030413
Cole Park <colepark2@outlook.com>
2019-11-08 18:43:21 -0600

    Build state tests for App.js and add functionality to App so that they pass

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,11 @@
+import axios from "axios";
+import { getLetterMatchCount } from "../helpers";
+
 export const actionTypes = {
 	CORRECT_GUESS: "CORRECT_GUESS",
 	GUESS_WORD: "GUESS_WORD",
 	SET_SECRET_WORD: "SET_SECRET_WORD"
 };
-
-import axios from "axios";
-import { getLetterMatchCount } from "../helpers";
 
 /**
 *@function correctGuess
